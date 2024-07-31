c509a5e1bdd014a357d07126fc4863dd29a0e5da
micael <micaelxv1@gmail.com>
2024-07-31 16:17:22 -0300

    feat: funcionando

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,11 @@
 import tipoArquivoRoute from './tipoArquivoRoute.js';
 import sistemaRoute from './sistemaRoute.js';
 import arquivoRoute from './arquivoRoute.js';
-import arquivoTeste from './arquivoTeste.js';
 
 function Routes(app) {
   tipoArquivoRoute(app);
   sistemaRoute(app);
   arquivoRoute(app);
-  arquivoTeste(app);
 }
 
 export default Routes;
